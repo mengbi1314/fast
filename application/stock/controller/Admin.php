3d9b2e3285c402627f2e176540cf9471a2e30e88
mengbi <mengbi1314@outlook.com>
2023-09-21 16:04:32 +0800

    H5端登录以及更新头像

--- application/stock/controller/Admin.php
+++ application/stock/controller/Admin.php
@@ -54,6 +54,7 @@ class Admin extends Controller
             'username' => $admin['username'],
             'nickname' => $admin['nickname'],
             'avatar_cdn' => $admin['avatar_cdn'],
+            'avatar' => $admin['avatar'],
             'email' => $admin['email'],
             'mobile' => $admin['mobile'],
             'group_text' => $admin['group_text'],
@@ -62,6 +63,7 @@ class Admin extends Controller
 
         $this->success('登录成功', null, $data);
     }
+
     public function unbinding()
     {
         $id = $this->request->param('id', '', 'trim');
@@ -124,6 +126,93 @@ class Admin extends Controller
             $this->error('绑定账号失败');
         } else {
             $this->success('绑定账号成功');
+        }
+    }
+
+    public function login()
+    {
+        $username = $this->request->param('username', '', 'trim');
+        $password = $this->request->param('password', '', 'trim');
+
+        $admin = $this->AdminModel->where(['username' => $username])->find();
+
+        if (!$admin) {
+            $this->error('账号不存在');
+        }
+
+        $password = md5(md5($password) . $admin['salt']);
+
+        if ($password != $admin['password']) {
+            $this->error('密码错误');
+        }
+
+        if ($admin['status'] !== 'normal') {
+            $this->error('当前账号已被禁用');
+        }
+
+        $data = [
+            'id' => $admin['id'],
+            'username' => $admin['username'],
+            'nickname' => $admin['nickname'],
+            'avatar_cdn' => $admin['avatar_cdn'],
+            'avatar' => $admin['avatar'],
+            'email' => $admin['email'],
+            'mobile' => $admin['mobile'],
+            'group_text' => $admin['group_text'],
+            'createtime' => $admin['createtime']
+        ];
+
+        $this->success('登录成功', null, $data);
+    }
+
+    public function upload()
+    {
+        $adminid = $this->request->param('adminid', '', 'trim');
+
+        $admin = $this->AdminModel->find($adminid);
+
+        if (!$admin) {
+            $this->error('当前管理员账号不存在');
+        }
+
+        if ($admin['status'] !== 'normal') {
+            $this->error('当前管理员账号已被禁用');
+        }
+
+        $res = build_upload('avatar');
+
+        if ($res['code'] === 0) {
+            $this->error($res['msg']);
+        }
+
+        $data = [
+            'id' => $adminid,
+            'avatar' => $res['data']
+        ];
+
+        $result = $this->AdminModel->isUpdate(true)->save($data);
+
+        if ($result === false) {
+            @is_file(ltrim($data['avatar'], '/')) && @unlink(ltrim($data['avatar'], '/'));
+            $this->error('更新头像失败');
+        } else {
+            @is_file(ltrim($admin['avatar'], '/')) && @unlink(ltrim($admin['avatar'], '/'));
+
+            $admin = $this->AdminModel->find($adminid);
+
+            $data = [
+                'id' => $admin['id'],
+                'username' => $admin['username'],
+                'nickname' => $admin['nickname'],
+                'avatar_cdn' => $admin['avatar_cdn'],
+                'avatar' => $admin['avatar'],
+                'email' => $admin['email'],
+                'mobile' => $admin['mobile'],
+                'group_text' => $admin['group_text'],
+                'createtime' => $admin['createtime']
+            ];
+
+            $this->success('更新头像成功', null, $data);
         }
     }
 
