0bf937bd7288b7c97a004c9d0160dafe6f13f9da
mengbi <mengbi1314@outlook.com>
2023-10-07 19:15:36 +0800

    React的修改资料

--- application/rent/controller/business/Base.php
+++ application/rent/controller/business/Base.php
@@ -83,4 +83,98 @@ class Base extends Controller
 
         $this->success('登录成功', null, $data);
     }
+
+    public function profile()
+    {
+        $params = $this->request->param();
+
+        $business = $this->BusinessModel->find($params['id']);
+
+        if (!$business) {
+            $this->error('用户不存在');
+        }
+
+        $data = [
+            'id' => $business['id'],
+            'nickname' => $params['nickname'],
+            'email' => $params['email'],
+            'gender' => $params['gender']
+        ];
+
+        if ($data['email'] != $business['email']) {
+            $data['auth'] = 0;
+        }
+
+        if (!empty($params['password'])) {
+            $repass = md5($params['password'] . $business['salt']);
+
+            if ($repass == $business['password']) {
+                $this->error('新密码不能与旧密码一致');
+            }
+
+            $salt = build_ranstr();
+
+            $password = md5($params['password'] . $salt);
+
+            $data['salt'] = $salt;
+            $data['password'] = $password;
+        }
+
+        if (!empty($params['code'])) {
+            $path = model('Region')->where(['code' => $params['code']])->value('parentpath');
+
+            if (!$path) {
+                $this->error('所选地区不存在');
+            }
+
+            [$province, $city, $district] = explode(',', $path);
+
+            $data['province'] = $province ?? null;
+            $data['city'] = $city ?? null;
+            $data['district'] = $district ?? null;
+        }
+
+        if (isset($_FILES['avatar']) && $_FILES['avatar']['size'] > 0) {
+            $res = build_upload('avatar');
+
+            if ($res['code'] === 0) {
+                $this->error($res['msg']);
+            }
+            $data['avatar'] = $res['data'];
+        }
+
+        //更新数据表
+        $result = $this->BusinessModel->validate('common/business/Business.profile')->isUpdate(true)->save($data);
+
+        if ($result === false) {
+            if (isset($data['avatar']) && $_FILES['avatar']['size'] > 0) {
+                @is_file(ltrim($data['avatar'], '/')) && @unlink(ltrim($data['avatar'], '/'));
+            }
+
+            $this->error($this->BusinessModel->getError());
+        } else {
+            if (isset($data['avatar']) && $_FILES['avatar']['size'] > 0) {
+                @is_file(ltrim($business['avatar'], '/')) && @unlink(ltrim($business['avatar'], '/'));
+            }
+
+            $business = $this->BusinessModel->find($params['id']);
+
+            $data = [
+                'id' => $business['id'],
+                'mobile' => $business['mobile'],
+                'mobile_text' => $business['mobile_text'],
+                'nickname' => $business['nickname'],
+                'email' => $business['email'],
+                'avatar_cdn' => $business['avatar_cdn'],
+                'gender' => $business['gender'],
+                'gender_text' => $business['gender_text'],
+                'region_text' => $business['region_text'],
+                'province' => $business['province'],
+                'city' => $business['city'],
+                'district' => $business['district'],
+                'auth' => $business['auth']
+            ];
+            $this->success('更新成功', null, $data);
+        }
+    }
 }
