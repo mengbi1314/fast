3bb676a25eedff8814d4c8992e8c0d4ca90e9bda
mengbi <mengbi1314@outlook.com>
2023-10-08 16:40:23 +0800

    React的学术页轮播图

--- application/rent/controller/Category.php
+++ application/rent/controller/Category.php
@@ -39,4 +39,16 @@ class Category extends Controller
             $this->error('暂无文章');
         }
     }
+
+    public function recommend()
+    {
+        if ($this->request->isPost()) {
+            // flag = recommend
+            $list = $this->CategoryModel->where(['flag' => ['LIKE', "%recommend%"]])->order('weigh DESC')->limit(8)->select();
+
+            if ($list) {
+                $this->success('查询成功', null, $list);
+            }
+        }
+    }
 }
