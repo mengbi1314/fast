b02da680bcc07e1cb91d9bff3e6f5c39dd439a72
mengbi <mengbi1314@outlook.com>
2023-10-11 19:06:22 +0800

    新品,热销

--- application/shop/controller/Index.php
+++ application/shop/controller/Index.php
@@ -16,13 +16,19 @@ class Index extends Controller
         $TypeList = model('product.Type')->order('weight DESC')->limit(8)->select();
 
         // 新品商品的数据
+        $newList = model('product.Product')->where(['flag' => 2])->order('createtime DESC')->limit(2)->select();
+
 
         // 热销商品的数据
+        $hotList = model('product.Product')->where(['flag' => 3])->order('createtime DESC')->limit(3)->select();
+
 
         // 组装数据
         $data = [
             'recommendList' => $recommendList,
-            'TypeList' => $TypeList
+            'TypeList' => $TypeList,
+            'newList' =>  $newList,
+            'hotList' => $hotList
         ];
 
         $this->success('查询数据成功', null, $data);
