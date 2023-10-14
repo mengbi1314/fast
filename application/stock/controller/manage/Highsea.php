d92fc5aa0210e08ceb72e706d12630c323b1d0a3
mengbi <mengbi1314@outlook.com>
2023-10-14 12:09:41 +0800

    客户公海

--- application/stock/controller/manage/Highsea.php
+++ application/stock/controller/manage/Highsea.php
@@ -3,82 +3,114 @@
 namespace app\stock\controller\manage;
 
 use think\Controller;
-use think\Db;
 
 class Highsea extends Controller
 {
-    // 客户模型
-    protected $BusinessModel = null;
 
-    // 管理员模型
     protected $AdminModel = null;
 
-    protected $Admin = [];
+    protected $BusinessModel = null;
+
+    protected $ReceiveModel = null;
 
     public function __construct()
     {
         parent::__construct();
 
-        $this->BusinessModel = model('business.Business');
         $this->AdminModel = model('admin.Admin');
 
-        $adminid = $this->request->param('adminid', '', 'trim');
+        $this->BusinessModel = model('business.Business');
 
-        $this->Admin = $this->AdminModel->find($adminid);
-
-        if (!$this->Admin) {
-            $this->error('当前管理员账号不存在');
-        }
-
-        if ($this->Admin['status'] !== 'normal') {
-            $this->error('当前管理员账号已被禁用');
-        }
+        $this->ReceiveModel = model('business.Receive');
     }
 
+    /**
+     * 显示资源列表
+     */
     public function index()
     {
-        // SELECT * FROM `pre_business` WHERE `adminid` IS NULL ORDER BY `createtime` DESC
-        $list = $this->BusinessModel->where(['adminid' => ['exp', Db::raw('IS NULL')]])->order('createtime DESC')->select();
+        $adminid = $this->request->param('adminid', 0, 'trim');
 
-        if ($list) {
-            $this->success('查询成功', null, $list);
+        $admin = $this->AdminModel->find($adminid);
+
+        if (!$admin) {
+            $this->error('管理员不存在');
+        }
+
+        if ($admin['status'] !== 'normal') {
+            $this->error('当前管理员账号已被禁用');
+        }
+
+        $result = $this->BusinessModel->with(['source'])->whereNull('adminid')->select();
+
+        if ($result === false) {
+            $this->error('暂无公海数据');
         } else {
-            $this->error('公海暂无客户');
+            $this->success('公海资源查询成功', null, $result);
         }
     }
 
+    //公海详情
     public function info()
-    {
-        $busid = $this->request->param('businessid', 0, 'trim');
-
-        $business = $this->BusinessModel->with(['source'])->find($busid);
-
-        if (!$business) {
-            $this->error('该客户不存在');
-        }
-
-        $this->success('查询成功', null, $business);
-    }
-
-    public function apply()
     {
         $busid = $this->request->param('busid', 0, 'trim');
 
         $business = $this->BusinessModel->find($busid);
 
         if (!$business) {
-            $this->error('客户不存在');
+            $this->error('用户不存在');
         }
 
-        $ReceiveModel = model('business.Receive');
+        $result = $this->BusinessModel->with(['source', 'region'])->find($busid);
+
+        if (!$result) {
+            $this->error('查询用户信息失败');
+        } else {
+            $this->success('查询用户信息成功', null, $result);
+        }
+    }
+
+    //查询管理员
+    public function admin()
+    {
+        $list = $this->AdminModel->select();
+
+        if (!$list) {
+            $this->error('暂无管理员');
+        } else {
+            $this->success('查询管理员成功', null, $list);
+        }
+    }
+
+    //客户公海分配
+    public function allot()
+    {
+        $adminid = $this->request->param('adminid', 0, 'trim');
+        $busid = $this->request->param('busid', 0, 'trim');
+
+        $admin = $this->AdminModel->find($adminid);
+
+        if (!$admin) {
+            $this->error('管理员不存在');
+        }
+
+        if ($admin['status'] !== 'normal') {
+            $this->error('当前管理员账号已被禁用');
+        }
+
+        $business = $this->BusinessModel->find($busid);
+
+        if (!$business) {
+            $this->error('用户不存在');
+        }
 
         // 开启事务
         $this->BusinessModel->startTrans();
-        $ReceiveModel->startTrans();
+        $this->ReceiveModel->startTrans();
 
         $BusinessData = [
-            'id' => $busid,
-            'adminid' => $this->Admin['id']
+            'id'        =>  $busid,
+            'adminid'   =>  $adminid
         ];
 
         $BusinessStatus = $this->BusinessModel->isUpdate(true)->save($BusinessData);
@@ -88,25 +120,86 @@ class Highsea extends Controller
         }
 
         $ReceiveData = [
-            'applyid' => $this->Admin['id'],
-            'status' => 'apply',
+            'applyid' => $adminid,
+            'status' => 'allot',
             'busid' => $busid
         ];
 
-        $ReceiveStatus = $ReceiveModel->validate('common/business/Receive')->save($ReceiveData);
+        $ReceiveStatus = $this->ReceiveModel->validate('common/business/Receive')->save($ReceiveData);
 
         if ($ReceiveStatus === false) {
             $this->BusinessModel->rollback();
-            $this->error($ReceiveModel->getError());
+            $this->error($this->ReceiveModel->getError());
         }
 
         if ($BusinessStatus === false || $ReceiveStatus === false) {
             $this->BusinessModel->rollback();
-            $ReceiveModel->rollback();
+            $this->ReceiveModel->rollback();
             $this->error('申请失败');
         } else {
             $this->BusinessModel->commit();
-            $ReceiveModel->commit();
+            $this->ReceiveModel->commit();
+            $this->success('申请成功');
+        }
+    }
+
+    //公海申请领取
+    public function apply()
+    {
+        $adminid = $this->request->param('adminid', 0, 'trim');
+        $busid = $this->request->param('busid', 0, 'trim');
+
+        $admin = $this->AdminModel->find($adminid);
+
+        if (!$admin) {
+            $this->error('管理员不存在');
+        }
+
+        if ($admin['status'] !== 'normal') {
+            $this->error('当前管理员账号已被禁用');
+        }
+
+        $business = $this->BusinessModel->find($busid);
+
+        if (!$business) {
+            $this->error('用户不存在');
+        }
+
+        // 开启事务
+        $this->BusinessModel->startTrans();
+        $this->ReceiveModel->startTrans();
+
+        $BusinessData = [
+            'id'        =>  $busid,
+            'adminid'   =>  $adminid
+        ];
+
+        $BusinessStatus = $this->BusinessModel->isUpdate(true)->save($BusinessData);
+
+        if ($BusinessStatus === false) {
+            $this->error('更新用户数据失败');
+        }
+
+        $ReceiveData = [
+            'applyid' => $adminid,
+            'status' => 'apply',
+            'busid' => $busid
+        ];
+
+        $ReceiveStatus = $this->ReceiveModel->validate('common/business/Receive')->save($ReceiveData);
+
+        if ($ReceiveStatus === false) {
+            $this->BusinessModel->rollback();
+            $this->error($this->ReceiveModel->getError());
+        }
+
+        if ($BusinessStatus === false || $ReceiveStatus === false) {
+            $this->BusinessModel->rollback();
+            $this->ReceiveModel->rollback();
+            $this->error('申请失败');
+        } else {
+            $this->BusinessModel->commit();
+            $this->ReceiveModel->commit();
             $this->success('申请成功');
         }
     }
