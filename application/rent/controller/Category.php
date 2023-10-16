a780689976f90b6ef84b998fdac1e49b93578ba3
mengbi <mengbi1314@outlook.com>
2023-10-16 15:48:59 +0800

    React-学术详情页

--- application/rent/controller/Category.php
+++ application/rent/controller/Category.php
@@ -43,12 +43,46 @@ class Category extends Controller
     public function recommend()
     {
         if ($this->request->isPost()) {
-            // flag = recommend
+            // flag = recommend 
             $list = $this->CategoryModel->where(['flag' => ['LIKE', "%recommend%"]])->order('weigh DESC')->limit(8)->select();
 
             if ($list) {
                 $this->success('查询成功', null, $list);
             }
         }
+    }
+
+    public function info()
+    {
+        $id = $this->request->param('id', 0, 'trim');
+        $busid = $this->request->param('busid', 0, 'trim');
+
+        $info = $this->CategoryModel->find($id);
+
+        if (!$info) {
+            $this->error('文章不存在');
+        }
+
+        $prev = $this->CategoryModel->where(['id' => ['<', $id]])->order('id DESC')->find();
+
+        $next = $this->CategoryModel->where(['id' => ['>', $id]])->order('id ASC')->find();
+
+        $business = model('business.Business')->find($busid);
+
+        $info['collection_status'] = false;
+
+        if ($business) {
+            $collection = model('business.Collection')->where(['busid' => $busid, 'cateid' => $id])->find();
+
+            $info['collection_status'] = $collection ? true : false;
+        }
+
+        $data = [
+            'prev' => $prev,
+            'info' => $info,
+            'next' => $next
+        ];
+
+        $this->success('查询成功', null, $data);
     }
 }
