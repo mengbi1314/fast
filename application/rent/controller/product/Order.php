a0148b62f3d01efefdcd009e90b3332a168835ce
mengbi <mengbi1314@outlook.com>
2023-10-17 12:00:40 +0800

    React-我的订单二

--- application/rent/controller/product/Order.php
+++ application/rent/controller/product/Order.php
@@ -17,5 +17,32 @@ class Order extends Controller
 
     public function index()
     {
+        $busid = $this->request->param('busid', 0, 'trim');
+        $page = $this->request->param('page', 1, 'trim');
+        $limit = $this->request->param('limit', 10, 'trim');
+        $status = $this->request->param('status', 0, 'trim');
+
+        $where = [
+            'busid' => $busid
+        ];
+
+        if ($status) {
+            $where['lease.status'] = $status;
+        }
+
+        $count = $this->LeaseModel->with(['product'])->where($where)->count();
+
+        $list = $this->LeaseModel->with(['product'])->where($where)->page($page, $limit)->select();
+
+        $data = [
+            'count' => $count,
+            'list' => $list,
+        ];
+
+        if ($list) {
+            $this->success('查询成功', null, $data);
+        } else {
+            $this->error('暂无订单');
+        }
     }
 }
